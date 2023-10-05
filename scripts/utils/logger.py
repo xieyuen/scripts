02d33a3849cc14793ced9e62c9383b09ee532615
xieyuen <107105640+xieyuen@users.noreply.github.com>
2023-10-05 16:47:30 +0800

    Update logger.py

--- scripts/utils/logger.py
+++ scripts/utils/logger.py
@@ -1,30 +1,51 @@
 import sys
 
-from loguru import logger
+from loguru._logger import Core, Logger
 
 
-logger.remove()
-logger.add(
-    sys.stdout,
-    format="[{time:YYYY-MM-DD HH:mm:ss}] [{level}] | {message}",
-    level="DEBUG",
-)
-logger.add(
-    "logs/latest.log",
-    format="[{time:YYYY-MM-DD HH:mm:ss}] [{level}] | {message}",
-    level="DEBUG",
-    rotation="1 week",
-    retention="100 days",
-    encoding="utf-8",
-)
+__all__ = ["ConsoleLogger", "logger"]
 
 
-trace = logger.trace
-debug = logger.debug
-info = logger.info
-success = logger.success
-warning = warn = logger.warning
-error = logger.error
-critical = crit = logger.critical
-catch = logger.catch
-exception = logger.exception
+class ConsoleLogger:
+    def __init__(self):
+        self._logger = Logger(
+            core=Core(),
+            exception=None,
+            depth=0,
+            record=False,
+            lazy=False,
+            colors=False,
+            raw=False,
+            capture=True,
+            patchers=[],
+            extra={},
+        )
+        self._logger.remove()
+        self._logger.add(
+            sys.stdout,
+            format="[{time:YYYY-MM-DD HH:mm:ss}] [{level}] | {message}",
+            level="DEBUG",
+        )
+        self._logger.add(
+            "logs/latest.log",
+            format="[{time:YYYY-MM-DD HH:mm:ss}] [{level}] | {message}",
+            level="DEBUG",
+            rotation="1 week",
+            retention="100 days",
+            encoding="utf-8",
+        )
+
+        self.trace = self._logger.trace
+        self.debug = self._logger.debug
+        self.info = self._logger.info
+        self.success = self._logger.success
+        self.warning = self._logger.warning
+        self.warn = self._logger.warning
+        self.error = self._logger.error
+        self.critical = self._logger.critical
+        self.crit = self._logger.critical
+        self.catch = self._logger.catch
+        self.exception = self._logger.exception
+
+
+logger = ConsoleLogger()
