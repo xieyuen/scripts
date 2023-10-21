65ad5c0ebd3afbf1097ddf22e19f7e9f90369792
xieyuen <107105640+xieyuen@users.noreply.github.com>
2023-10-21 19:02:13 +0800

    Update randomnum.py

--- scripts/utils/randomnum.py
+++ scripts/utils/randomnum.py
@@ -67,20 +67,21 @@ class MainProgram:
         if self.runtimes is not None:
             self.runtimes = int(self.runtimes)
         self.ignore_list = (
-                eval(self.args.get('--ignore-list', '[]'))
-                + eval(self.args.get('--ignore', '[]'))
+            eval(self.args.get('--ignore-list', '[]'))
+            + eval(self.args.get('--ignore', '[]'))
         )
         self.disable_dedup = str2bool(self.args.get('--disable-dedup', 'false'))
         self.enable_save = (
-                str2bool(self.args.get('--save', 'false'))
-                or str2bool(self.args.get('-s', 'false'))
+            str2bool(self.args.get('--save', 'false'))
+            or str2bool(self.args.get('-s', 'false'))
         )
         self.enable_cli = (
-                str2bool(self.args.get('--enable-cli', 'false'))
-                or str2bool(self.args.get('--enable-console', 'false'))
+            str2bool(self.args.get('--enable-cli', 'false'))
+            or str2bool(self.args.get('--enable-console', 'false'))
         )
         self.enable_map = str2bool(self.args.get('--enable-map', 'false'))
-        self.map = eval(self.args.get('--map', 'None'))  # TODO: 从文件读取映射表 考虑支持 .py .json .yaml .txt
+        if self.enable_map:
+            self.map = eval(self.args.get('--map', 'None'))  # TODO: 从文件读取映射表 考虑支持 .py .json .yaml .txt
 
         self.__check_config()
 
@@ -91,13 +92,13 @@ class MainProgram:
             if self.runtimes < 1:
                 raise ValueError('The runtimes must be greater than 0.')
             if self.runtimes > (self.max - self.min + 1) and not self.disable_dedup:
-                raise ValueError('Argument:runtimes grater than Argument:max - Argument:min + 1 but enable dedup')
+                raise ValueError('Arg:runtimes grater than Arg:max - Arg:min + 1 but enable dedup')
         if self.enable_map:
             if not isinstance(self.map, dict):
-                raise TypeError('Argument:map must be a dict.')
+                raise TypeError('Arg:map must be a dict.')
             if not all(isinstance(i, int) for i in self.map.keys()):
                 if not all(isinstance(i, int) for i in self.map.values()):
-                    raise TypeError('Argument:map keys must be int.')
+                    raise TypeError('Arg:map keys must be int.')
                 logger.warning('It seems that the map key is inverse with the value?')
                 logger.warning('Program will reverse the key to the value.')
                 self.map = {v: k for k, v in self.map.items()}
@@ -114,8 +115,8 @@ class MainProgram:
     def __main(self) -> int:
         r = ri(self.min, self.max)
         while (
-                (not self.disable_dedup and r in self.new)
-                or r in self.ignore_list
+            (not self.disable_dedup and r in self.new)
+            or r in self.ignore_list
         ):
             r = ri(self.min, self.max)
         self.new.append(r)
