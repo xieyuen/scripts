0119c79b57b42110bdc5671db3b0e5ac034887ee
xieyuen <107105640+xieyuen@users.noreply.github.com>
2023-10-22 15:05:18 +0800

    新增: `FileReader`

---
+++ scripts/utils/file_reader.py
@@ -0,0 +1,60 @@
+import json
+from typing import TextIO, Optional, Callable
+from pathlib import Path
+
+import yaml
+
+from scripts.utils.jsobj import JSObject
+
+
+class FileReader:
+    constants = JSObject()
+    constants.DEFAULT_CODING = 'utf-8'
+    constants.TYPE_MAP = JSObject(
+        yaml='yaml', yml='yaml',
+        text='text', txt='text',
+        json='json',
+    )
+    TYPES = constants.TYPE_MAP
+
+    def __init__(self, path: str, file_type: str = None):
+        self.file = Path(path)
+        self.file_name = self.file.name
+        self.file_type = self.constants.TYPE_MAP[file_type.lower()]
+
+    def change_file_type(self, value: str):
+        self.file_type = self.constants.TYPE_MAP[value.lower()]
+
+    def read(self, reader: Optional[Callable] = None, *, encoding='utf-8', load_to_pyobj_first=False):
+        """
+        读取文件, 若传入 reader 则会将文件流传给 reader 并返回 reader 的返回
+        若生成实例时未传入 file_type, 则返回包含文件内容的字符串
+
+        Args:
+            reader: Optional[Callable]          | 内容处理
+            encoding: str ='utf-8'              | 编码
+            load_to_pyobj_first: bool = False   | 是否先将文件内容转换为 python 对象再传给 reader
+
+        Returns: Any
+        """
+        if reader is not None:
+            with self.file.open(encoding=encoding) as f:
+                if load_to_pyobj_first:
+                    return reader(self.__read(f, self.file_type))
+                return reader(f)
+        return self.__read(self.file.open(encoding=encoding), self.file_type)
+
+    @staticmethod
+    def __read(file_io: TextIO, file_type: str) -> object:
+        match file_type:
+            case None:
+                return file_io.read()
+
+            case FileReader.TYPES.yaml:
+                data = dict()
+                for datas in yaml.safe_load_all(file_io):
+                    data.update(datas)
+                return data
+
+            case FileReader.TYPES.json:
+                return json.load(file_io)
