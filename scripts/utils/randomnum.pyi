7c4f558802aec5d668ff122e2104c3d30485c1cd
xieyuen <107105640+xieyuen@users.noreply.github.com>
2023-11-24 23:22:24 +0800

    Update randomnum.pyi

--- scripts/utils/randomnum.pyi
+++ scripts/utils/randomnum.pyi
@@ -1,28 +1,33 @@
 from _typeshed import Incomplete
-from scripts.utils.file_reader import FileReader as FileReader
-from scripts.utils.logger import logger as logger
-from scripts.utils.string_to import str2bool as str2bool
+from os import PathLike
+from typing import List, Dict, Optional, Any
 
 __version__: str
 
+
 class MainProgram:
-    args: Incomplete
-    last: Incomplete
-    new: Incomplete
-    max: Incomplete
-    min: Incomplete
-    runtimes: Incomplete
-    ignore_list: Incomplete
-    enable_increase: Incomplete
-    increase_list: Incomplete
-    disable_dedup: Incomplete
-    enable_save: Incomplete
-    enable_cli: Incomplete
-    enable_map: Incomplete
-    map: Incomplete
-    encoding: Incomplete
-    map_file: Incomplete
+    args: Dict[str, str]
+    last: List[int]
+    new: List[int]
+    max: int
+    min: int
+    runtimes: Optional[int]
+    ignore_list: List[int]
+    enable_increase: bool
+    increase_list: List[int]
+    disable_dedup: bool
+    enable_save: bool
+    enable_cli: bool
+    enable_map: bool
+    map: Dict[int, Any]
+    encoding: str
+    map_file: str
     def __init__(self, args) -> None: ...
     def run(self, runtimes: Incomplete | None = ...) -> None: ...
+    def __load_map_from_file(self) -> None: ...
+    def __main(self) -> int: ...
+    def __check_config(self) -> None: ...
+    def __save(self, name: str = ...) -> None: ...
+    def __cli(self) -> None: ...
 
 def main(args): ...
