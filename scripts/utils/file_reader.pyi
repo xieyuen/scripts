b3167c8f72500d004f17454c1f7a0286f68a97d4
xieyuen <107105640+xieyuen@users.noreply.github.com>
2023-11-24 23:22:19 +0800

    Update file_reader.pyi

--- scripts/utils/file_reader.pyi
+++ scripts/utils/file_reader.pyi
@@ -1,13 +1,40 @@
 from _typeshed import Incomplete
-from scripts.utils.jsobj import JSObject as JSObject
-from typing import Any, Callable, Optional
+from pathlib import Path
+
+from scripts.utils.jsobj import JSObject
+from typing import Any, Callable, Optional, TextIO, IO
+
 
 class FileReader:
-    constants: Incomplete
-    TYPES: Incomplete
-    file: Incomplete
-    file_name: Incomplete
-    file_type: Incomplete
-    def __init__(self, path: str, file_type: str = ...) -> None: ...
+    __consts: JSObject
+    TYPES: JSObject
+    __file: Path
+    __file_name: str
+    __file_type: str
+    __file_io: Optional[IO[Any]]
+    def __init__(self, path: str, file_type: str = ...) -> None:         ...
     def change_file_type(self, value: str): ...
-    def read(self, reader: Optional[Callable] = ..., *, encoding: str = ..., load_to_pyobj_first: bool = ...) -> Any: ...
+    def read(
+            self,
+            reader: Optional[Callable] = ...,
+            *,
+            encoding: str = ...,
+            load_to_pyobj_first: bool = ...
+    ) -> Any: ...
+    @staticmethod
+    def __read(file_io: TextIO, file_type: str) -> Any: ...
+    def open(
+            self,
+            mode: str,
+            buffering: int = ...,
+            encoding: str | None = ...,
+            errors: str | None = ...,
+            newline: str | None = ...,
+            closefd: bool = ...,
+            opener: Callable[[str, int] ,int] | None = ...,
+    ) -> IO[Any]: ...
+    class __States:
+        isOpened: bool
+        isClosed: bool
+        isExists: bool
+        def __init__(self, file_reader): ...
