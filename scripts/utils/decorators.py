f746025084672d678c83a382583d56b12f6c4148
xieyuen <107105640+xieyuen@users.noreply.github.com>
2023-11-26 13:37:29 +0800

    Update decorators.py

--- scripts/utils/decorators.py
+++ scripts/utils/decorators.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, Tuple, Literal, Any, Optional, NoReturn
 
 
 def print_return(callback: Callable) -> Callable:
@@ -14,14 +14,14 @@ def print_return(callback: Callable) -> Callable:
     return wrapper
 
 
-def add_return_code(callback: Callable) -> Callable:
+def add_return_code(callback: Callable) -> Callable[[...], Tuple[Literal[0, 1], Any]]:
     """
     Decorator for printing the return code of a function.
     if an exception(as e) is raised, it will return (1, e)
     if not, it will return (0, result)
     """
 
-    def wrapper(*args, **kwargs):
+    def wrapper(*args, **kwargs) -> Tuple[Literal[0, 1], Any]:
         try:
             result = callback(*args, **kwargs)
         except Exception as e:
@@ -30,3 +30,32 @@ def add_return_code(callback: Callable) -> Callable:
             return 0, result
 
     return wrapper
+
+
+def print_return_code(callback: Callable) -> Callable:
+    """
+    Decorator for printing the return code of a function.
+    """
+
+    class Wrapper:
+        def __init__(self, __call):
+            self.__e = None  # type: Optional[Exception]
+            self.__callback = __call
+
+        def __call__(self, *args, **kwargs):
+            try:
+                result = self.__callback(*args, **kwargs)
+                code = 0
+            except Exception as e:
+                print(f'Callable: {self.__callback.__name__} raises an exception.')
+                code = 1
+                self.__e = e
+            print(f'Return code of callable: {self.__callback.__name__} is {code}')
+            return result
+
+        def get_exception(self) -> Exception | NoReturn:
+            if self.__e is None:
+                raise RuntimeError('No Exception is raised.')
+            return self.__e
+
+    return Wrapper(callback)
