805677f92d1dc2ffda29b91c701829c503d08f7a
xieyuen <107105640+xieyuen@users.noreply.github.com>
2024-01-11 22:02:56 +0800

    Music Crawler 2.0!

--- scripts/crawler/music.py
+++ scripts/crawler/music.py
@@ -1,7 +1,12 @@
+import os
+from typing import TypedDict, Literal
+
 import requests
 from jsonpath import jsonpath
 
+from scripts.utils import dedup
 from scripts.utils.decorators import curry
+from scripts.utils.exceptions import MusicNotFoundError, PathNotExistsError
 
 
 class Constants:
@@ -9,23 +14,23 @@ class Constants:
     defaultSavePath = './'
     searchURL = 'https://music.liuzhijin.cn/'
     headers = {
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
+        "user-agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/87.0.4280.141 Safari/537.36",
         # 判断请求是异步还是同步
         "x-requested-with": "XMLHttpRequest",
     }
     platformInfo = (
-        "脚本支持以下平台:\n"
         "1.网易云:netease\n"
         "2.QQ:qq\n"
         "3.酷狗:kugou\n"
         "4.酷我:kuwo\n"
         "5.百度:baidu\n"
-        "6.喜马拉雅:ximalaya\n"
-        "请选择平台: "
+        "6.喜马拉雅:ximalaya"
     )
     platformMap = {
-        k: v for keys, v in
-        (
+        k: v for keys, v in (
             (
                 (
                     '1', 'n', 'net', 'wy', 'wyy', 'wangyi', 'wangyiyun', 'netease', '网易', '网易云', '网易云音乐'
@@ -51,13 +56,14 @@ class Constants:
                     '6', 'x', 'xi', 'xmly', 'xmla', 'ximalaya', '喜马拉雅'
                 ), 'xiamlaya'
             ),
-        )
-        for k in keys
+        ) for k in keys
     }
 
 
-class MusicNotFoundError(Exception):
-    pass
+class ParamType(TypedDict):
+    input: str
+    filter: Literal['name']
+    type: Literal[*dedup(Constants.platformMap.values())]
 
 
 class Crawler:
@@ -71,18 +77,22 @@ class Crawler:
         }
 
     @staticmethod
-    def getMusicContentData(url: str) -> bytes:
-        return requests.get(url).content
+    @curry
+    def download(url, author, title, *, path=Constants.defaultSavePath):
+        if hasattr(Constants, 'savePath') and Constants.defaultSavePath != Constants.savePath:
+            path = Constants.savePath
 
-    def download(self, url, author, title, path=Constants.defaultSavePath):
         print(f'{author}-{title} 正在下载...')
-        with open(f"{path}/{author}-{title}.mp3", mode='wb') as f:
-            f.write(self.getMusicContentData(url))
+        with open(f"{path}/{title}-{author}.mp3", mode='wb') as f:
+            f.write(requests.get(url).content)
 
     def main(self):
+        download = self.download
+
         param = self.getParam(
             name=input("请输入歌曲名:"),
-            platform=input(Constants.platformInfo)
+            platform=input(
+                f"脚本支持以下平台:\n{Constants.platformInfo}\n请选择平台: ")
         )
         json_text = requests.post(
             url=Constants.searchURL,
@@ -102,11 +112,19 @@ class Crawler:
             print(f"{index + 1} | {t} - {a}")
 
         indexes = input("请输入您想下载的歌曲版本(填序号,多个用英文逗号隔开):").split(',')
-        savePath = input("请输入保存路径(空则为脚本所在路径):")
-        if savePath:
-            download = curry(self.download)(path=savePath)
+
+        if not (
+                hasattr(Constants, 'savePath')
+                and Constants.defaultSavePath != Constants.savePath
+        ):
+            savePath = input("请输入保存路径(空则为脚本所在路径):")
         else:
-            download = self.download
+            savePath = None
+
+        if savePath:
+            if not os.path.exists(savePath):
+                raise PathNotExistsError('不存在的路径')
+            download = download(path=savePath)
 
         for i in indexes:
             i = int(i) - 1
@@ -114,4 +132,13 @@ class Crawler:
 
 
 if __name__ == '__main__':
+    import sys
+
+    args = {
+        key: value
+        for item in sys.argv[1:]
+        for key, value in [item.split('=')]
+    }
+    if '--path' in args:
+        Constants.savePath = args['--path']
     Crawler().main()
